f77dd9a76f6d574f9de7eed8814a84d84463af11
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-12-07 11:33:31 +0530

    Day 4 Terraform State file management with AWS S3

--- lessons/day04/code/main.tf
+++ lessons/day04/code/main.tf
@@ -1,5 +1,15 @@
 # Configure the AWS Provider
 terraform {
+
+  backend "s3" {
+     bucket         = "terraformuser-backup-state"
+     key            = "dev/terraform.tfstate"
+     region         = "ap-south-2"
+     use_lockfile   = true
+     encrypt        = true
+   }
+
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -10,33 +20,43 @@ terraform {
 
 provider "aws" {
   # Configuration options
-    region = "ca-central-1"
+    region = "ap-south-2"
 }
 
 # backend configuration
-terraform {
-  backend "s3" {
-    bucket         = "erraform-state-1754513244"
-    key            = "dev/terraform.tfstate"
-    region         = "ca-central-1"
-    use_lockfile  = "true"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "erraform-state-1754513244"
+#     key            = "dev/terraform.tfstate"
+#     region         = "ca-central-1"
+#     use_lockfile  = "true"
+#     encrypt        = true
+#   }
+# }
 
 
 # Simple test resource to verify remote backend
-resource "aws_s3_bucket" "test_backend" {
-  bucket = "test-remote-backend-${random_string.bucket_suffix.result}"
+# resource "aws_s3_bucket" "test_backend" {
+#   bucket = "test-remote-backend-${random_string.bucket_suffix.result}"
+
+#   tags = {
+#     Name        = "Test Backend Bucket"
+#     Environment = "dev"
+#   }
+# }
+
+# resource "random_string" "bucket_suffix" {
+#   length  = 8
+#   special = false
+#   upper   = false
+# }
+
+# Create a S3 bucket
+resource "aws_s3_bucket" "tf_test_rakesh_bucket" {
+  bucket = "my-tf-test-rakesh-bucket"
 
   tags = {
-    Name        = "Test Backend Bucket"
-    Environment = "dev"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
-}
-
-resource "random_string" "bucket_suffix" {
-  length  = 8
-  special = false
-  upper   = false
 }
